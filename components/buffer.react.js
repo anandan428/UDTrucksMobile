8eaceccbc4760d1e4b91b41cbfd76fe6bff5f61e
Nandan A <anandan428@gmail.com>
2018-07-06 09:36:11 +0530

    intermediate UI

---
+++ components/buffer.react.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+    View,
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity
+} from 'react-native';
+
+
+export default class Buffers extends React.Component {
+    render(){
+        return(
+            <Text>Hello Order Screen</Text>
+        );
+    }
+}
+
+
+
---
+++ components/parts.react.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+    View,
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity
+} from 'react-native';
+
+
+export default class Parts extends React.Component {
+    render(){
+        return(
+            <Text>Hello Order Screen</Text>
+        );
+    }
+}
+
+
+
